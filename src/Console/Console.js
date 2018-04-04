09c82c0ac208ae2f4977c5375183d12067f76825
Sam Moss <sam.moss321@gmail.com>
2018-04-03 19:57:13 -0500

    long range radar renders target points

--- src/Console/Console.js
+++ src/Console/Console.js
@@ -6,7 +6,7 @@ class Console extends Component {
   constructor() {
     super();
     this.state = {
-      location: { x: 100, y: 100 }
+      location: [{ x: 100, y: 100 }]
     };
     this.createLongRangeRadar = this.createLongRangeRadar.bind(this);
   }
@@ -15,6 +15,7 @@ class Console extends Component {
   }
 
   createLongRangeRadar() {
+    const { location } = this.state;
     const node = this.node;
     const yScale = scaleLinear()
       .domain([-500, 500])
@@ -23,13 +24,50 @@ class Console extends Component {
       .domain([-500, 500])
       .range([0, 500]);
 
-    select(node).append("rect");
+    let screen = select(node)
+      .append("g")
+      .attr("class", "screen");
 
-    // select(node)
-    //   .selectAll("rect")
-    //   .attr("width", 500)
-    //   .attr("height", 500)
-    //   .style("fill", "rgb(33, 54, 51)");
+    screen
+      .append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("rx", 15)
+      .attr("ry", 15)
+      .attr("width", 500)
+      .attr("height", 500)
+      .attr("fill", "url(#Gradient1)")
+      .attr("class", "field");
+
+    let targets = select(node)
+      .append("g")
+      .attr("class", "targets");
+
+    targets
+      .selectAll("rect")
+      .data(location)
+      .enter()
+      .append("rect");
+
+    targets
+      .selectAll("rect")
+      .data(location)
+      .exit()
+      .remove();
+
+    targets
+      .selectAll("rect")
+      .data(location)
+      .style("fill", "#8bb3b2")
+      .attr("class", "target")
+      .attr("x", function(d) {
+        return xScale(d.x);
+      })
+      .attr("y", function(d) {
+        return yScale(d.y);
+      })
+      .attr("width", 2)
+      .attr("height", 2);
   }
 
   render() {
@@ -37,20 +75,10 @@ class Console extends Component {
       <svg ref={node => (this.node = node)} width={500} height={500}>
         <defs>
           <linearGradient id="Gradient1" x1="0" x2="1" y1="0" y2="1">
-            <stop offset="50%" stop-color="rgb(33, 54, 51)" />
+            <stop offset="55%" stop-color="rgb(33, 54, 51)" />
             <stop offset="100%" stop-color="rgb(97, 134, 137)" />
           </linearGradient>
         </defs>
-
-        <rect
-          x="0"
-          y="0"
-          rx="15"
-          ry="15"
-          width="500"
-          height="500"
-          fill="url(#Gradient1)"
-        />
       </svg>
     );
   }
